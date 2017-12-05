585367bc2cd8913160e7a419745c7582d0515b40
Wilkinson <andrew.wilkinson@sky.uk>
2017-12-05 15:46:52 +0000

    ObjectPoolFactory

---
+++ SimpleStart - Respawn Timer/Assets/Scripts/Editor/ObjectPoolFactoryEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(ObjectPoolFactory))]
+    [CanEditMultipleObjects]
+    public class ObjectPoolFactoryEditor : UnityEditor.Editor
+    {
+        public ObjectPoolFactory PoolFactory;
+
+        private void OnEnable()
+        {
+            PoolFactory = (ObjectPoolFactory) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+            
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Object To Pool", GUILayout.Width(175));
+            PoolFactory.ObjectToAdd =
+                (GameObject) EditorGUILayout.ObjectField(PoolFactory.ObjectToAdd, typeof(Object), true);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Add Object"))
+            {
+                PoolFactory.AddObjectToBePooled(PoolFactory.ObjectToAdd);
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Object Pooler Name: ", GUILayout.Width(175));
+            PoolFactory.PoolName = GUILayout.TextArea(PoolFactory.PoolName);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            PoolFactory.PoolQuantity = EditorGUILayout.IntField("# Of Each Object To Pool:", PoolFactory.PoolQuantity);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            PoolFactory.EnableGrowth = EditorGUILayout.Toggle("Enable Dynamic Pool Growth: ", PoolFactory.EnableGrowth);
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.Space();
+            
+            if (GUILayout.Button("Generate Pooling Object"))
+            {
+                PoolFactory.GeneratePoolingObject();
+            }
+            
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            
+            if (GUILayout.Button("Clear All"))
+            {
+                PoolFactory.ClearAllOptions();
+            }
+        }
+    }
+}
---
+++ SimpleStart - Respawn Timer/Assets/Scripts/ObjectPoolFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolFactory : MonoBehaviour
+{
+    public GameObject PoolSkeleton;
+
+    public List<GameObject> ObjectsToPool = new List<GameObject>();
+    public string PoolName { get; set; }
+    public int PoolQuantity { get; set; }
+    public bool EnableGrowth { get; set; }
+    public GameObject ObjectToAdd { get; set; }
+
+    public void AddObjectToBePooled(GameObject obj)
+    {
+        obj.SetActive(false);
+        ObjectsToPool.Add(obj);
+    }
+
+    public void GeneratePoolingObject()
+    {
+        GameObject objPooler = Instantiate(PoolSkeleton);
+        objPooler.name = PoolName;
+        var script = objPooler.GetComponent<ObjectPool>();
+        script.EnableGrowth = EnableGrowth;
+        script.PoolQuantity = PoolQuantity;
+        script.ObjectsToPool = ObjectsToPool;
+
+        script.ContructPooler();
+    }
+
+    public void ClearAllOptions()
+    {
+        ObjectsToPool.Clear();
+        ObjectToAdd = null;
+        PoolName = "";
+        EnableGrowth = false;
+        PoolQuantity = 0;
+    }
+}
